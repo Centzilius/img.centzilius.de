c0f7263995200bf2f47c89d354f3f298ca9c5315
donaukind <nero@w1r3.net>
2013-07-25 20:53:14 +0000

    nero is trying to use git...

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <?php
-if ($h = opendir('./')) {
+/* pushen auf git:
+git add %filename%
+git commit -m 'worked on code' 
+git push
+*/
+
+if (isset($_GET['thumbnail'])) {
+  $fn=$_GET['thumbnail'];
+  $fn=str_replace("\\/|<>","",$fn);
+  if (file_exists($fn)) {
+  var_dump($fn);
+
+  }
+}
+?>
+
+<table>
+<tr><th>Preview</th><th>Filename</th></tr>
+<?php
+if ($h = opendir(__DIR__.'/images')) {
 	while (false !== ($e = readdir($h))) {
-		if ($e != '.' /* && $e != '..'*/) {
-			echo "<a href=\"$e\">$e</a>";
+		if ($e[0]!='.') {
+			echo "<tr><td><img src=\"images/".$e."\" width=100 /></td><td><a href=\"$e\">$e</a></td></tr>".PHP_EOL;
 		} 
 	}
 } else {
 	echo 'Nothing is here yet :(';
 }
 ?>
+</table>
